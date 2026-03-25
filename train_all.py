c8d4a784684922943fc74c35b6acae9375932843
Viktor <Viktor Szolga>
2026-03-25 02:32:06 +0100

    Deleted old files

--- train_all.py
+++ train_all.py
@@ -4,7 +4,11 @@ import os
 
 if __name__=="__main__":
     datasets = ["ml-1m", "beauty", "steam", "ml-20m"]
+    models = ["bert", "nmf"]
+    seeds = list(range(5))
     for dataset in datasets:
         subprocess.run([sys.executable, "-u", os.path.join("preprocessing", f"{dataset}.py")], check=True)
-        subprocess.run([sys.executable, "-u", "train.py", "-m", "nmf", "-d", f"{dataset}"], check=True)
+        for model in models:
+            for seed in seeds:
+                subprocess.run([sys.executable, "-u", "train.py", "-m", f"{model}", "-d", f"{dataset}", "-s", f"{seed}"], check=True)
     
